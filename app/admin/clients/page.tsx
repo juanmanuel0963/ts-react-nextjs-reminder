efa94fca1442a08e62b3819477aa89c7c80758c7
juanmanuel0963 <juanmanuel0963@gmail.com>
2024-04-22 11:47:39 -0500

    commit

--- app/admin/clients/page.tsx
+++ app/admin/clients/page.tsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 import {
   Form,
   FormControl,
